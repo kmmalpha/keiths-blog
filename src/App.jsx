30fe27b422aafcefdc40e1f150e379a2584de4ff
Keith Mdu Mwelaze <keithndlovu4@gmail.com>
2023-10-19 20:24:28 +0200

    navbar added and design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Blog from './pages/Blog';
@@ -8,14 +8,13 @@ import Blog from './pages/Blog';
 const App = () => {
 	return (
 		<Router>
-			<div>
-				<Header />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/blog" element={<Blog />} />
-				</Routes>
-			</div>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/blog" element={<Blog />} />
+				<Route path="*" element={<Navigate to="/" />} />
+			</Routes>
 		</Router>
 	);
 }
---
+++ src/pages/Blog.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles/blog.css';
+
+const Blog = () => {
+  return (
+    <></>
+  )
+}
+
+export default Blog
